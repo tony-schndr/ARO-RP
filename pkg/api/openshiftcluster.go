ab47a5dfb621b4955c6e545af1c22ef9867125e7
tschneid <tschneid@redhat.com>
2023-06-21 13:45:04 -0500

    add LoadbalancerProfile to internal API

--- pkg/api/openshiftcluster.go
+++ pkg/api/openshiftcluster.go
@@ -261,6 +261,44 @@ const (
 	OutboundTypeLoadbalancer       OutboundType = "Loadbalancer"
 )
 
+// LoadbalancerProfile represents the profile of the cluster public loadbalancer.
+type LoadbalancerProfile struct {
+	// The desired managed outbound IPs for the cluster public load balancer.
+	ManagedOutboundIPs ManagedOutboundIPs `json:"managedOutboundIps,omitempty"`
+	// The list of effective outbound IP addresses of the public load balancer.
+	EffectiveOutboundIPs []EffectiveOutboundIP `json:"effectiveOutboundIps,omitempty"`
+	// The list of resource ids for customer provided Outbound IP Addresses to be added to the cluster public loadbalancer.
+	OutboundIPs []OutboundIP `json:"outboundIps,omitempty"`
+	// The list of resource ids for customer provided Outbound IP Prefixes to be added to the cluster public loadbalancer.
+	OutboundIPPrefixes []OutboundIPPrefix `json:"OutboundIpPrefixes,omitempty"`
+	// OutboundRuleSNAT determines the number of SNAT ports to allocate per instance in outbound rule backend pool.
+	OutboundRuleSNAT int `json:"outboundRuleSNAT,omitempty"`
+}
+
+// EffectiveOutboundIP represents an effective outbound IP resource of the cluster public load balancer.
+type EffectiveOutboundIP struct {
+	// The ResourceID of the effective outbound IP Address.
+	ID string `json:"id,omitempty"`
+}
+
+// ManagedOutboundIPs represents the desired managed outbound IPs for the cluster public load balancer.
+type ManagedOutboundIPs struct {
+	// Count represents the desired number of IPv4 outbound IPs created/managed by Azure for the cluster public load balancer.
+	Count int `json:"count,omitempty"`
+}
+
+// OutboundIP represents a customer provided outbound IP to be added to the cluster public loadbalancer.
+type OutboundIP struct {
+	// The ResourceID of the outbound IP Address.
+	ID string `json:"id,omitempty"`
+}
+
+// OutboundIPPrefix represents a customer provided outbound IP prefix to be added to the cluster public loadbalancer.
+type OutboundIPPrefix struct {
+	// The ResourceID of the outbound IP prefix.
+	ID string `json:"id,omitempty"`
+}
+
 // NetworkProfile represents a network profile
 type NetworkProfile struct {
 	MissingFields
@@ -270,6 +308,7 @@ type NetworkProfile struct {
 	SoftwareDefinedNetwork SoftwareDefinedNetwork `json:"softwareDefinedNetwork,omitempty"`
 	MTUSize                MTUSize                `json:"mtuSize,omitempty"`
 	OutboundType           OutboundType           `json:"outboundType,omitempty"`
+	LoadbalancerProfile    LoadbalancerProfile    `json:"loadBalancerProfile,omitempty"`
 
 	APIServerPrivateEndpointIP string           `json:"privateEndpointIp,omitempty"`
 	GatewayPrivateEndpointIP   string           `json:"gatewayPrivateEndpointIp,omitempty"`
